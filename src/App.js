1d675692f6ff1589a1ee9c462f354bbc3f5aaad8
ChusnulHadi <office.chusnulhadi@gmail.com>
2022-05-27 11:19:42 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import Model from './3d_tree-processed';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Canvas
+        camera={{ position: [1, 0, 12.25], fov: 15 }}
+      >
+        <OrbitControls enableZoom={false} maxPolarAngle={2}/>
+        <ambientLight intensity={1} />
+        <directionalLight intensity={0.5} />
+        <Model position={[0, -1, 0]} />
+      </Canvas>
     </div>
   );
 }
